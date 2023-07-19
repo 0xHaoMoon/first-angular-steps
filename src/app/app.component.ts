6d8bdc864b06c39ddec4dbebab47a00b998d611b
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-07-19 19:19:39 +0200

    add images & start work on slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'haotruong';
+export class AppComponent implements OnInit{
+  images = ['lucario.jpg','mewtu.jpg','dark.jpg'];
+  curentImage = 0;
+  showImage = true;
+
+  ngOnInit(){
+    this.updateImage();
+  }
+
+  updateImage(){
+    
+   setInterval(()=>{
+    this.curentImage++;
+    this.curentImage = this.curentImage % this.images.length;
+    this.showImage = false;
+
+    setTimeout(() => {
+      this.showImage = true;
+    }, 10);
+   },8000);
+  }
+
 }
